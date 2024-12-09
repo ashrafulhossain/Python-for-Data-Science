e49c838bb3756dec45d79dd227ad0a2dce507e5e
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-09 15:34:45 +0600

    work done

---
+++ input and output.py
@@ -0,0 +1,20 @@
+# process 1
+num1 = 10
+num2 = 20
+print('The number are:', num1, num2)
+
+country = 'Bangladesh'
+team = 'Cricket Team'
+
+print(country, end=' ')
+print(team)
+
+# process 2
+country1 = 'Bangladesh'
+country2 = 'Singapur'
+country3 = 'Nepal'
+print(f"Country one is {country1}. Country two is {country2}. Country three is {country3}")
+
+# process 3
+num = 20.3047394
+print(f"num is {num:.2f}")
