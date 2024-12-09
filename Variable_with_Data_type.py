e3b1b51ead5f2de24702bec6955059314097eec2
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-09 15:21:02 +0600

    work done

---
+++ Variable_with_Data_type.py
@@ -0,0 +1,26 @@
+# Declaring variable
+
+language = " python"
+
+# Rule 1: A variable name must start with a letter on the underscore character
+
+age = 27
+
+# Rule 2: A Variable name can't start with a number
+
+# Rule 3: A variable name can only contain alpha-numeric(A-Za, z0-9) characters and underscore
+first_name = 'Ashraful'
+
+# Rule 4: variable name are case sensitive
+name = "Pyhton"
+NAME = "Python"
+Name = "Python"
+
+# Rule 5: The reserved keyword can't be used variable name
+help('keywords')
+
+# Rule 6: And you can assign the same value to multiple variables in one line:
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
