059fb264f8fba0cf0f450b74ebd6a4997d278753
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-09 15:53:02 +0600

    work done

---
+++ operators.py
@@ -0,0 +1,7 @@
+# Python divides the operators in the following groups:
+# Arithmetic operators : +, -, *, /, %, **, //.
+# Assignment Operators : =, +=, -=, *=, /=, %=, //=, >>=, <<=.
+# Comparison Operators : ==, !=, >, < , >=, <=.
+# Logical Operators : and, or, not.
+# Identity Operators : is, is not.
+# Membership Operators : in, not in
