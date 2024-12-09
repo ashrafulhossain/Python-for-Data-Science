9a1f2cddf10326aadd57719597de5c13a83966ce
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-09 23:49:46 +0600

    work done

--- list.py
+++ list.py
@@ -1,3 +1,20 @@
 # Lists are used to store multiple items in a single variable.
 # Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
 # Lists are created using square brackets
+
+marks = [3, 5, 6]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+
+
+marks = [3, 5, 6, 'Ashraf', True]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+print(marks[3])
+print(marks[4])
