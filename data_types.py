c464c9d561264bd7a00b3a9c2c2c4898612a2ac7
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-09 15:31:55 +0600

    work done

---
+++ data_types.py
@@ -0,0 +1,15 @@
+# Variables can store data of different types, and different types can do different things.
+x = "Hello World" #str
+y = 20 #int
+z = 20.5 #float
+a =  ["apple", "banana", "cherry"] #List
+b = ("apple", "banana", "cherry") #tupple
+c = range(6) #range
+d = {"name" : "John", "age" : 36} #dictonary
+e = {"apple", "banana", "cherry"} #set
+f = True #bool
+n = None #none
+
+type # data type checking
+
+print(type(x))
