1720ae55e47079640e42288f8532c1fbd39efc23
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-10 19:59:25 +0600

    work done

---
+++ f_string.py
@@ -0,0 +1,7 @@
+uv = "i am courrently study at {}. are you {}? ok thanku. i am from {}. my fabourite hobby{}"
+versity = "Bgc Trust University"
+good = "fine"
+country = "Chattogram"
+hobbies = "Cricket"
+
+print(f"i am courrently study at {versity}. are you {good}? ok thanku. i am from {country}. my fabourite hobby{hobbies}")
