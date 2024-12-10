110e8bbb26f5f59abbd87c67e706bbc88d157a94
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-10 18:13:19 +0600

    work done

--- dictonaries.py
+++ dictonaries.py
@@ -1,3 +1,25 @@
 # Dictionaries are used to store data values in key:value pairs.
 # A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
 # Dictionaries are written with curly brackets, and have keys and values:
+
+dic = {
+    'Harry': 'Huming Being',
+    'Spon': 'Object'
+}
+print(dic)
+print(type(dic))
+
+dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(dict)
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(thisdict)
