98ba8dc02396439cb16eee1e5f4d15ff3ddb82d5
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-11 19:45:37 +0600

    work done

--- for_loops.py
+++ for_loops.py
@@ -125,6 +125,20 @@ else:
   print("Finally finished!")
 
 
+takeNumber = input("Phone : ")
+digits = {
+    "1":"One",
+    "2" : "Two",
+    "3":"Three",
+    "4" : "Four"
+}
+
+output =""
+for ch in takeNumber:
+  output += digits.get(ch, "!") + " "
+print(output)
+
+
 
 
 
