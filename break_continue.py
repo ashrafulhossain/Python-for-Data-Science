a7881a3a2aab94979c2858409e94b7bcf23dd9e8
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-12 20:57:30 +0600

    work done

--- break_continue.py
+++ break_continue.py
@@ -11,3 +11,11 @@ for i in range(12):
         print('skip the itteration')
         continue
     print("5 X", i+1, "=", 5 * (i + 1))
+
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
