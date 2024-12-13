3c13ca029d81c57106e5aa1b2525960d8a967e66
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-13 20:46:17 +0600

    work done

--- functions.py
+++ functions.py
@@ -66,3 +66,19 @@ character = isGreater(50, 40)
 print(character)
 
 
+def is_prime(num):
+
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+number = 29
+print(f"Is {number} prime? {is_prime(number)}")
+
+
+
+
+
