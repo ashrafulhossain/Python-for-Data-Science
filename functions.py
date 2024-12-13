ef5d35bae175d1a426b4beb1b3cd8249a6e6b507
ashrafulhossain <ashrafulhossainrrr@gmail.com>
2024-12-13 20:48:47 +0600

    work done

--- functions.py
+++ functions.py
@@ -85,4 +85,13 @@ print("Reversed string:", reverse_string(text))
 
 
 
+def factorial(n):
+
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+# Example usage
+number = 5
+print(f"Factorial of {number}:", factorial(number))
 
